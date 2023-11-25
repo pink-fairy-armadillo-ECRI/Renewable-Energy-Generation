5a1fa626659e84b4da5825af8e41689ce9004309
Final4Head <jeffreymai3000@gmail.com>
2023-11-25 10:13:27 -0500

    frontend branch

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import marketsReducer from './marketsReducer';
+import dataReducer from './dataReducer';
 
 const reducers = combineReducers({
   // if we had other reducers, they would go here
