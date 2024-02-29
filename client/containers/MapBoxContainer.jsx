ec77abf7a16fd34619ef57784c14d3709a665691
Hugh Stapleton <hstapleton808@gmail.com>
2024-02-29 07:42:38 -0500

    progress on mapbox

--- client/containers/MapBoxContainer.jsx
+++ client/containers/MapBoxContainer.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+
+
+const MapBoxContainer = () => {
+
+
+    return(
+        <div className="mapBoxContainer">
+
+        </div>
+    )
+}
+
+export default MapBoxContainer;
